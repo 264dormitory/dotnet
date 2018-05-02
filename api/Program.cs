f2c86a3a0e8e8f3143980c8e5f618ad9c38676ee
高黎明 <xp01255410@icloud.com>
2018-05-03 00:54:54 +0800

    初始化后台api

---
+++ api/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+
+
+            //带端口号的启动命令
+            //dotnet run --server.urls "http://localhost:8081"
+    }
+}
