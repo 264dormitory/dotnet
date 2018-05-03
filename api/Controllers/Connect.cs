660da968f70eb98b1ed11e0923789dfc1121ea88
高黎明 <xp01255410@icloud.com>
2018-05-04 00:18:36 +0800

    飞机api完成

--- api/Controllers/Connect.cs
+++ api/Controllers/Connect.cs
@@ -38,7 +38,32 @@ namespace api{
             //返回json数据
             return result;
         }
+        
+        public static string DeleteStringResultWithId(string route) {
+            //定义返回的数据
+            string result = "";
 
+            //定义请求头
+            string url    = Connect.CONNECT_INFO + route;   
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);  
+            request.Method = "DELETE";
+            request.ContentType = "json";
+
+            //获取内容
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream stream = response.GetResponseStream();
+            using (StreamReader reader = new StreamReader(stream, Encoding.UTF8))  
+            {  
+                result = reader.ReadToEnd();  
+            } 
+
+            //返回json数据
+            return result;
+        }
+
+        /**
+         * 根据POST绑定实体返回信息
+         */
         public static string PostStringResultWithRequestBody(string route, byte[] data) {
             //定义返回数据
             string result ="";
---
+++ api/Controllers/PlaneController.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.IO;
+using System.Text;
+using System.Net.Http;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace api.Controllers
+{
+    public class PlaneController : Controller
+    {
+        // todo 跨域问题
+        [Route("/User/login")]
+        [HttpPost]
+        public JsonResult Login([FromBody]User value) {
+            //模拟发送http请求 POST数据
+            //将User对象转化为json数据格式
+            //定义请求的数据
+            byte[] bytes = Encoding.UTF8.GetBytes(value.ToString());
+
+            //返回json数据
+            return Json(Connect.PostStringResultWithRequestBody("/User/login", bytes));
+        }
+
+        //获取所有的飞机信息的分页信息
+        [Route("/Plane/pageAllPlanes")]
+        [HttpGet]
+        public JsonResult PageAllPlanes() {
+
+            //返回json数据
+            return Json(Connect.GetStringResultWithNoParams("/Plane/pageAllPlanes"));
+        }
+
+        // 删除飞机
+        [Route("/Plane/{id}")]
+        [HttpDelete]
+        public JsonResult Delete(int id) {
+            //返回json数据
+            return Json(Connect.GetStringResultWithNoParams("/Plane/" + id));
+        }
+
+        //保存飞机实体
+        [Route("/Plane")]
+        [HttpPost]
+        public JsonResult save([FromBody] Plane plane) {
+            //模拟发送http请求 POST数据
+            //将User对象转化为json数据格式
+            //定义请求的数据
+            byte[] bytes = Encoding.UTF8.GetBytes(plane.ToString());
+
+            //返回json数据
+            return Json(Connect.PostStringResultWithRequestBody("/Plane", bytes));
+        }
+    }
+}
