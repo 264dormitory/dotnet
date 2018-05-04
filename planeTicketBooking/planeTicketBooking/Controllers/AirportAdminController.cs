b0400b1d74c61cab4116ea2618c2ed3aebe80c01
Lishjie <851275924@qq.com>
2018-05-04 16:21:03 +0800

    完成后台机场名称设置数据绑定

--- planeTicketBooking/planeTicketBooking/Controllers/AirportAdminController.cs
+++ planeTicketBooking/planeTicketBooking/Controllers/AirportAdminController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using planeTicketBooking.Models;
+using System.Data;
+using MySql.Data.MySqlClient;  //导入连接使用MySQL数据库所必须的命名空间
 /// <summary>
 /// 李世杰创建
 /// 
@@ -21,12 +23,52 @@ namespace planeTicketBooking.Controllers
         // GET: 绑定机场名称设置页面的视图
         public ActionResult airportname_set()
         {
-            City city = new City();
-            city.CityId = 1;
-            city.CityName = "北京市";
-            city.AirportName = "首都T2机场";
-            ViewData["city"] = city;
-            return View();
+            CityListModels cityList = new CityListModels();
+            //建立City Model列表
+            List<City> cityArray = new List<City>();
+            //连接数据库
+            MySqlConnection conn = new MySqlConnection();
+            conn.ConnectionString = "Server=localhost;Database=ticket;User ID=admin;Password=admin;port=3306;CharSet=utf8;pooling=true;SslMode=None;";
+            //获取City数据表中的所有数据
+            string cityMsg = String.Format("select * from city");
+            MySqlCommand comm = new MySqlCommand(cityMsg, conn);
+            //创建和初始化数据适配器DataAdapter
+            MySqlDataAdapter cityAdapter = new MySqlDataAdapter(cityMsg, conn);
+            //创建DataSet对象
+            DataSet cityDataSet = new DataSet();
+            try
+            {
+                conn.Open();
+                cityAdapter.Fill(cityDataSet, "city");  //将DataSet起名为city
+                //使用DataTable来提取DataSet表中的数据
+                DataTable cityDataTable = cityDataSet.Tables["city"];
+                
+                //将DataTable中的数据赋值到List之中
+                for (int row = 0; row < cityDataTable.Rows.Count; row++)
+                {
+                    City city = new City();
+                    city.CityId = Convert.ToInt16(cityDataTable.Rows[row]["id"]);
+                    city.CityName = cityDataTable.Rows[row]["city_name"].ToString();
+                    city.AirportName = cityDataTable.Rows[row]["airport_name"].ToString();
+                    cityArray.Add(city);
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+            finally
+            {
+                cityList.CityArray = cityArray;
+                conn.Close();
+            }
+            return View(cityList);
+        }
+
+        //用于实现数据库删除
+        public void delete()
+        {
+
         }
     }
 }
---
+++ planeTicketBooking/planeTicketBooking/Models/CityListModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+/// <summary>
+/// 李世杰添加city列表类
+/// </summary>
+namespace planeTicketBooking.Models
+{
+    public class CityListModels
+    {
+        public List<City> CityArray { get; set; }
+    }
+}
