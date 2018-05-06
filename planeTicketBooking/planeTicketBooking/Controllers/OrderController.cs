70340f734b91435afedc8d8b812cbc82d8f54844
han2048030 <510569451@qq.com>
2018-05-06 22:16:37 +0800

    临时提交

--- planeTicketBooking/planeTicketBooking/Controllers/OrderController.cs
+++ planeTicketBooking/planeTicketBooking/Controllers/OrderController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 /// <summary>
-/// 李世杰创建
+///韩瑞峰修改18.5.6
 /// 
 /// 用于：
 /// 1、订单填写页面
@@ -18,7 +18,15 @@ namespace planeTicketBooking.Controllers
     public class OrderController : Controller
     {
         // GET: Order
-        public ActionResult Index()
+        public ActionResult order()
+        {
+            return View();
+        }
+    }
+    public class OrderFullController : Controller
+    {
+        // GET: Order
+        public ActionResult orderfull()
         {
             return View();
         }
